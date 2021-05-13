b5c5eef09299ae021f8231dba21db8804514fc4a
Juhász Krisztián István <juhasz.krisztian2000@gmail.com>
2021-05-13 19:19:58 +0200

    Added basic menus

--- FoodMeasureApp/Form1.cs
+++ FoodMeasureApp/Form1.cs
@@ -16,5 +16,16 @@ namespace FoodMeasureApp
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void addNewFoodToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newFood1.Visible = true;
+            pictureBox1.Visible = false;
+        }
     }
 }
